a754cd46083176e7e1a1803926580ebd918b273e
Artikunazo <artik16032011@gmail.com>
2023-07-04 00:10:11 -0600

    [IMP] Remove ng-bootstrap. [FT] Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CoreModule } from '@core/core.module';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { CoreModule } from '@core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     CoreModule,
   ],
   providers: [],
